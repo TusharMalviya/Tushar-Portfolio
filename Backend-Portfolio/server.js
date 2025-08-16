347f233f83aae720c79832d9a5432e03d6a65383
shreya-hegdee <shreyahegde850@gmail.com>
2025-08-16 13:42:20 +0530

    cors solution

--- Backend-Portfolio/server.js
+++ Backend-Portfolio/server.js
@@ -13,18 +13,8 @@ require('dotenv').config(); // Load environment variables
 // Server setup
 const app = express();
 // app.use(cors());
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: process.env.ALLOWED_ORIGIN,// Allow requests from React (localhost:3000)
 }));
 
 app.use(express.json());
